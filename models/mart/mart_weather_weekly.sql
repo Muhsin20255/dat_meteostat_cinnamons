44caf2f7fd595ad970521e346ad8ff701623496b
Muhsin20255 <mukhsidin95@gmail.com>
2026-02-13 11:41:05 +0100

    mart weather

--- models/mart/mart_weather_weekly.sql
+++ models/mart/mart_weather_weekly.sql
@@ -1,12 +1,10 @@
-/* 5.4 Weekly Weather Statistics */
-
 SELECT
     airport_code,
     DATE_TRUNC('week', date) AS weather_week,
 
     AVG(avg_temp_c) AS avg_weekly_temp,
     AVG(avg_wind_speed_kmh) AS avg_weekly_wind_speed,
-    SUM(total_precipitation_mm) AS total_weekly_precipitation
+    SUM(precipitation_mm) AS total_weekly_precipitation
 
 FROM {{ ref('prep_weather_daily') }}
 
