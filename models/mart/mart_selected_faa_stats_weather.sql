0b574b4c087581017414c4ce637c6ca11670b83c
Muhsin20255 <mukhsidin95@gmail.com>
2026-02-13 11:36:36 +0100

    mart selected

--- models/mart/mart_selected_faa_stats_weather.sql
+++ models/mart/mart_selected_faa_stats_weather.sql
@@ -3,8 +3,8 @@ SELECT
     DATE_TRUNC('week', date) AS weather_week,
 
     AVG(avg_temp_c) AS avg_weekly_temp,
-    AVG(avg_wind_speed) AS avg_weekly_wind_speed,
-    SUM(total_precipitation) AS total_weekly_precipitation
+    AVG(avg_wind_speed_kmh) AS avg_weekly_wind_speed,
+    SUM(total_precipitation_mm) AS total_weekly_precipitation
 
 FROM {{ ref('prep_weather_daily') }}
 
